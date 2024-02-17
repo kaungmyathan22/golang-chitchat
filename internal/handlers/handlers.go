ea4f97fae8b2e20984cfea59ba0c2ada58f7c974
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-17 14:20:56 +0630

    feat(): remvoe user when leve the page

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -84,6 +84,14 @@ func ListenToWsChannel() {
 			response.Action = "list_users"
 			response.ConnectedUsers = users
 			broadcastToAll(response)
+		case "left":
+			// handle the situation where a user leaves the page
+			response.Action = "list_users"
+			delete(clients, e.Conn)
+			users := getUserList()
+			response.ConnectedUsers = users
+			broadcastToAll(response)
+
 		}
 		response.Action = "Got here"
 		response.Message = fmt.Sprintf("Some message , and action was %s", e.Action)
