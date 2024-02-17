fe5d55cd9697076a7bc5c6ce9d716e088d8dfa85
Kaung Myat Han <kaungmyathan2222@gmail.com>
2024-02-17 14:07:42 +0630

    feat(): ws connection listener added

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,24 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/kaungmyathan22/golang-chitchat/internal/handlers"
 )
 
 func main() {
 	mux := routes()
 
 	log.Println("Starting channel listener")
-	// go handlers.ListenToWsChannel()
+	go handlers.ListenToWsChannel()
 
 	log.Println("Staring web server on port 8080")
 
-	_ = http.ListenAndServe(":8080", mux)
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 }
